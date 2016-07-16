ea92c10074f3c05c8335f471b01e3f23cdf69d59
suliac <sle-guil@student.42.fr>
2016-07-16 18:46:45 +0200

    update

--- Colleen/colleen.c
+++ Colleen/colleen.c
@@ -1,4 +1,4 @@
 #include<stdio.h>
 /*comment*/
 void f();
-int main(){/*comment*/char*t="#include<stdio.h>%1$c/*comment*/%1$cvoid f();%1$cint main(){/*comment*/char*t=%3$c%4$s%3$c;printf(t,10,9,34,t);}%1$c";printf(t,10,9,34,t);}
+int main(){/*comment2*/char*t="#include<stdio.h>%1$c/*comment*/%1$cvoid f();%1$cint main(){/*comment2*/char*t=%3$c%4$s%3$c;printf(t,10,9,34,t);}%1$c";printf(t,10,9,34,t);}
