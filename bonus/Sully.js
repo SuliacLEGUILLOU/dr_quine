066fced30b2c73ba11a1ea7dc0896b299dce3384
suliac <sle-guil@student.42.fr>
2016-07-27 13:02:54 +0200

    wip, C Sully bug

--- bonus/Sully.js
+++ bonus/Sully.js
@@ -1,7 +1,6 @@
-var n = 5,fs = require('fs'),exec = require('child_process');
+var n=5,fs=require('fs'),exec=require('child_process');
 (function f(){//comment
-    if (n > 1) {
-        fs.writeFile("Sully_" + (n - 1).toString() + ".js", "var n = " + (n - 1).toString() + ",fs = require('fs'),exec = require('child_process');\n(" + f.toString() + ")();");
-        exec.spawn("/usr/local/bin/node", ["Sully_" + (n - 1).toString() + ".js"]);    // Change the exec path
-    }
-})();
+if (n > 1) {
+fs.writeFile("Sully_"+n.toString()+".js", "var n="+(n-1).toString()+",fs=require('fs'),exec=require('child_process');\n("+f.toString()+")();");
+exec.spawn("/usr/local/bin/node",["Sully_"+n.toString()+".js"]);    // Change the exec path
+}})();
